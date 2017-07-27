7c81ae253a73b3eb12cf064752e4468d9e93c0c4
Lukas <lukasborges@gmail.com>
2017-07-27 16:02:45 -0400

    Outputs value attribute instead of while json.

--- parser.cpp
+++ parser.cpp
@@ -1,71 +1,91 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "json.h"
 
 using namespace std;
 
+//https://en.wikibooks.org/wiki/JsonCpp
+
+class mesh
+{
+	public:
+		string meshType;
+};
+
 void Indent(ostream& ofs, int indent) {
-	for (int i = 0; i<indent; i++)
-		ofs << ' ';
+	for (int i = 0; i < indent; i++)
+		//ofs << ' ';
+		break;
 }
 
-void MyPrint(ostream& ofs, const Json::Value& val, int indent = 0) {
+void MyPrint(ostream& ofs, const Json::Value& val, vector<mesh>& v, int indent = 0) {
 	switch (val.type()) {
-	case Json::nullValue: ofs << "null"; break;
-	case Json::booleanValue: ofs << (val.asBool() ? "true" : "false"); break;
-	case Json::intValue: ofs << val.asLargestInt(); break;
-	case Json::uintValue: ofs << val.asLargestUInt(); break;
-	case Json::realValue: ofs << val.asDouble(); break;
+	case Json::nullValue: "null"; break;
+	case Json::booleanValue: (val.asBool() ? "true" : "false"); break;
+	case Json::intValue: val.asLargestInt(); break;
+	case Json::uintValue: val.asLargestUInt(); break;
+	case Json::realValue: val.asDouble(); break;
 	case Json::stringValue: {
 		// analyze String value of attribute:
 		string res = val.asString();
 		if (res == "Expr") {
-			cout << ":::found an Expr";
+			//cout << ":::found an Expr";
+			mesh expr;
+			expr.meshType = "Expr";
+			v.push_back(expr);
 		}
 		else if (res == "FunctionDef")
 		{
-			cout << ":::found a FunctionDef";
+			//cout << ":::found a FunctionDef";
+			mesh fdef;
+			fdef.meshType = "FunctionDef";
+			v.push_back(fdef);
 		}
 		else if (res == "Assign")
 		{
-			cout << ":::found an Assign";
+			//cout << ":::found an Assign";
+			mesh asgn;
+			asgn.meshType = "Assign";
+			v.push_back(asgn);
 		}
-		ofs << '"' << res << '"';
+		//ofs << '"' << res << '"';
 
 	} break;
 	case Json::arrayValue: {
 		Json::ArrayIndex size = val.size();
 		if (size == 0)
-			ofs << "[]";
+			break;
 		else {
-			ofs << "[\n";
+			//ofs << "[\n";
 			int newIndent = indent + 4;
 			for (Json::ArrayIndex i = 0; i<size; i++) {
-				Indent(ofs, newIndent);
-				MyPrint(ofs, val[i], newIndent);
-				ofs << (i + 1 == size ? "\n" : ",\n");
+				//Indent(ofs, newIndent);
+				MyPrint(ofs, val[i], v, newIndent);
+				//ofs << (i + 1 == size ? "\n" : ",\n");
 			}
-			Indent(ofs, indent);
-			ofs << ']';
+			//Indent(ofs, indent);
+			//ofs << ']';
 		}
 		break;
 	}
 	case Json::objectValue: {
 		if (val.empty())
-			ofs << "{}";
+			//ofs << "{}";
+			break;
 		else {
-			ofs << "{\n";
+			//ofs << "{\n";
 			int newIndent = indent + 4;
 			vector<string> keys = val.getMemberNames();
 			for (size_t i = 0; i<keys.size(); i++) {
-				Indent(ofs, newIndent);
+				//Indent(ofs, newIndent);
 				const string& key = keys[i];
-				ofs << '"' << key << '"' << " : ";
-				MyPrint(ofs, val[key], newIndent);
-				ofs << (i + 1 == keys.size() ? "\n" : ",\n");
+				//ofs << '"' << key << '"' << " : ";
+				MyPrint(ofs, val[key], v, newIndent);
+				//ofs << (i + 1 == keys.size() ? "\n" : ",\n");
 			}
 			Indent(ofs, indent);
-			ofs << '}';
+			//ofs << '}';
 		}
 		break;
 	}
@@ -76,11 +96,19 @@ void MyPrint(ostream& ofs, const Json::Value& val, int indent = 0) {
 }
 
 int main() {
-	ifstream ifs("file2.json");
+	vector<mesh> vec;
+	ifstream ifs("./file1.json");
 	Json::Value val;
 	ifs >> val;
-	MyPrint(cout, val);
+	MyPrint(cout, val, vec);
 	cout << '\n';
+
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		cout << vec.at(i).meshType << endl;
+	}
+
+
 	system("PAUSE");
 	return 0;
 }
